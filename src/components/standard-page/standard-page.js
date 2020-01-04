ba3cba82c499c5d22d0e2d3ae6270a05586f0fc3
José Manuel Lucas <josemanuel@jmlweb.es>
2020-01-05 00:39:18 +0100

    Style changes

--- src/components/standard-page/standard-page.js
+++ src/components/standard-page/standard-page.js
@@ -8,6 +8,7 @@ import styles from './standard-page.module.css';
 const StandardPage = ({ title, htmlTitle, children }) => (
   <div className={styles.wrapper}>
     <Seo title={title} />
+    <div className={styles.picWrapper} />
     <Header />
     <main className={styles.main}>
       <h1 className={styles.title}>{htmlTitle || title}</h1>
